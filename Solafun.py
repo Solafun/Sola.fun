91db9b1259ba67f7537fffdf2df4abb29a34c5ef
Sola.fun <71711340+Solafun@users.noreply.github.com>
2025-03-18 22:29:07 +0300

    Solafun.py

---
+++ Solafun.py
@@ -0,0 +1,10 @@
+#This file has no features, please review the README.MD file.
+
+print("Please read README.MD file for details!")
+
+print("Please read README.MD file for details!")
+
+print("buy script - https://app.hel.io/pay/67d9c4c51940965fc86ea835")
+
+print("buy script - https://app.hel.io/pay/67d9c4c51940965fc86ea835")
+
